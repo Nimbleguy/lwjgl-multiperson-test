fe5a4c6a080744ba7ff13ec66b7d373c6b98bfcb
Nimbleguy <nomblest@gmail.com>
2016-12-05 21:42:06 -0500

    Fixed stuff.

--- src/core/gl/RenderMovingObject.java
+++ src/core/gl/RenderMovingObject.java
@@ -4,11 +4,22 @@ public class RenderMovingObject implements IRenderTask{
 
 	private int prior = 2;
 
+	public RenderMovingObject(int p){
+		prior = p;
+	}
+
 	public int priority(){
 		return prior;
 	}
 
 	public boolean remove(){
 		return false;
+	}
+
+	public void init(){
+		
+	}
+	public void run(long window){
+		
 	}
 }
